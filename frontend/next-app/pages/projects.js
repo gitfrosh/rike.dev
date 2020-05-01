76a361cedaab2396517a502b5e3a26975df203ad
Rike <hello@ueberdiespree.de>
2020-05-01 21:26:49 +0200

    add texts

--- frontend/next-app/pages/projects.js
+++ frontend/next-app/pages/projects.js
@@ -12,25 +12,26 @@ function Projects() {
       </span>
       <section>
         <p className="project-info">
-          <b>Bot on Tap</b>. You need help setting up your own Twitter bot? We
-          are here to help you developing and even running and maintaining it as
-          "bot as a service".
+          <b>Bot on Tap</b>. Need help setting up your own Twitter bot? We
+          are here to help you setting it up as "bot as a service". <em>coming soon</em>
         </p>
 
         <p className="project-info">
-          <b>The Lord of the Rings API</b>. lorem ipsum.
+          <b>The Lord of the Rings API</b> is the biggest JSON based data source providing 
+          information about the trilogy.
         </p>
 
         <p className="project-info">
-          <b>The friendly @femtech_ bot</b>
+          <b>The friendly @femtech_ bot</b> retweets and supports female developers, scientists and 
+          engineers on Twitter.
         </p>
 
         <p className="project-info">
-          <b>queersplitter.de</b>. lorem
+          <b>queersplitter.de</b> is a German collaborative blog on LGBTIQ history.
         </p>
 
         <p className="project-info">
-          <b>the false quote generator</b>. fun project
+          <b>the false quote generator</b> is a fun project doing exactly what its name suggests.
         </p>
       </section>
     </>
