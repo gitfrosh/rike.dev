24228d31e0fd5d81b74d009da41621e8240ca489
rike <hello@rike.dev>
2021-03-02 19:46:06 +0100

    add name to title

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ class RootIndex extends React.Component {
         <Layout location={this.props.location}>
           <Helmet>
             <html lang="en, de" />
-            <title>rike.dev - Web development and beyond</title>
+            <title>rike.dev - Web development and beyond | Ulrike Exner</title>
             <meta name="description" content="I'm ulrike (or just rike) and I am a software developer from Berlin, Germany with a focus on web technologies." />
           </Helmet>
           <PageTitleHome image={this.props.data.file.childImageSharp.fluid} />
