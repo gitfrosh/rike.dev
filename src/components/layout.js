b3c614609647b8b3b16fa4089f77a5feb75d6f14
rike <hello@rike.dev>
2021-04-12 12:04:29 +0200

    cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,12 +10,6 @@ import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies';
 class Layout extends React.Component {
   render() {
     const { location, children } = this.props
-    // let header
-
-    // let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
 
     return (
       <Fragment>
