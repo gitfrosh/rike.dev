abe7a74e84e3afc042277c55ba7188ff1287ee00
rike <hello@rike.dev>
2021-05-03 22:30:54 +0200

    put advertisement for course project online

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,34 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';   
 
-const projects = [
-    {
-        "id": "1",
-        "title": "The Lord of the Rings API",
-        "description": "is the biggest JSON based data source providing information about the trilogy.",
-        "url": "https://the-one-api.dev/"
-    },
-    {
-        "id": "2",
-        "title": "minako",
-        "description": "is a super lightweight headless, API-first, flat-file cms written in Node.js/Next.js made for blogs and other small content-driven frontends.",
-        "url": "https://github.com/gitfrosh/minako"
-    },
-    {
-        "id": "3",
-        "title": "The friendly @femtech_ bot",
-        "description": "retweets and supports female developers, scientists and engineers on Twitter.",
-        "url": "https://twitter.com/femtech_"
-    },
-    {
-        "id": "4",
-        "title": ".. more",
-        "description": "on Github!",
-        "url": "https://github.com/gitfrosh/"
+const getBrowserLanguage = () => {
+    if (typeof navigator === `undefined`) {
+        return "en";
     }
-]
+    const lang = navigator && navigator.language && navigator.language.split("-")[0];
+    if (!lang) return "en";
+
+    switch (lang) {
+        case "de":
+            return "de";
+        default:
+            return "en";
+    }
+};
 
 
 const Projects = () => {
+    const [lang] = useState(getBrowserLanguage())
+    const [projects, setProjects] = useState([])
+    useEffect(() => {
+        console.log(lang)
+        if (lang === "de") {
+            setProjects([{
+                "id": "1",
+                "title": "The Lord of the Rings API",
+                "description": "is the biggest JSON based data source providing information about the trilogy.",
+                "url": "https://the-one-api.dev/"
+            },
+            {
+                "id": "2",
+                "title": "Dein Weg in die Web-Entwicklung",
+                "description": "NEU! Lerne alles, was du für einen erfolgreichen Einstieg in die Webentwicklung brauchst in 2 x 6 Wochen. Nur für Frauen!",
+                "url": "https://webentwicklung-kurse-fuer-frauen.rike.dev/",
+                "featured": true,
+            },
+            {
+                "id": "3",
+                "title": "The friendly @femtech_ bot",
+                "description": "retweets and supports female developers, scientists and engineers on Twitter.",
+                "url": "https://twitter.com/femtech_"
+            },
+            {
+                "id": "4",
+                "title": ".. more",
+                "description": "on Github!",
+                "url": "https://github.com/gitfrosh/"
+            }])
+
+        } else {
+            setProjects([{
+                "id": "1",
+                "title": "The Lord of the Rings API",
+                "description": "is the biggest JSON based data source providing information about the trilogy.",
+                "url": "https://the-one-api.dev/"
+            },
+            {
+                "id": "2",
+                "title": "minako",
+                "description": "is a super lightweight headless, API-first, flat-file cms written in Node.js/Next.js made for blogs and other small content-driven frontends.",
+                "url": "https://github.com/gitfrosh/minako"
+            },
+            {
+                "id": "3",
+                "title": "The friendly @femtech_ bot",
+                "description": "retweets and supports female developers, scientists and engineers on Twitter.",
+                "url": "https://twitter.com/femtech_"
+            },
+            {
+                "id": "4",
+                "title": ".. more",
+                "description": "on Github!",
+                "url": "https://github.com/gitfrosh/"
+            }])
+        }
+    }, [lang])
     return (
         <div id="awards" className="block spacer p-top-xl">
             <div className="wrapper">
@@ -44,7 +90,7 @@ const Projects = () => {
                             return (
                                 <div key={key} className="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                                     <div className="card">
-                                        <div className="card-body p-large">
+                                        <div style={{ border: item.featured && '5px solid #ff1e52' }} className="card-body p-large">
                                             <h4><a href={item.url}>{item.title}</a></h4>
                                             <p className="mt-0">{item.description}</p>
                                         </div>
