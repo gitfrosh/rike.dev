5704f66077a21dbf8bfd58d98c2f1b80fe81466b
gitfrosh <hello@rike.dev>
2026-06-22 10:23:30 +0200

    add plausible

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,16 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=IBM+Plex+Sans:wght@300;400;500&family=JetBrains+Mono:wght@400&display=swap"
           rel="stylesheet"
         />
+        {/* Privacy-friendly analytics by Plausible */}
+        <script
+          async
+          src="https://plausible.io/js/pa-x18ejzOBgiZP4164GIS_o.js"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()`,
+          }}
+        />
       </Head>
       <body>
         <Main />
